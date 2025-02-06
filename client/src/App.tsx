1e01e4e6f54c8e2142e8b39b6b793ef9b06f4e25
Shehryar M <shehryarm1029@gmail.com>
2025-02-06 14:46:10 -0500

    Quick .map study

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,11 +1,25 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import axios from "axios";
 import "./App.css";
 
 function App() {
+  // observe this object: {"members": ["Member1", "Member2", "Member3"]}
+  // it has a key called members that = an array
+  // when i initalize my object i need to make sure that this key exists, that way we won't get any errors
+  const [userData, setUserData] = useState({ members: [] });
+
+  useEffect(() => {
+    axios.get("http://127.0.0.1:5000/members").then((response) => {
+      setUserData(response.data);
+    });
+  }, []);
+
   return (
     <>
-      <div></div>
+      {/* A .map function was used because it modifies an array and returns a new one */}
+      {userData.members.map((member, index) => (
+        <div key={index}>{member}</div>
+      ))}
     </>
   );
 }
--- flask-server/server.py
+++ flask-server/server.py
@@ -1,6 +1,9 @@
 from flask import Flask
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app)  # Enable CORS for all routes
+
 
 
 @app.route("/members")
