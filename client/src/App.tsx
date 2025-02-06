f7947b97abbc688d317acdd233a4b01190fcecdd
Shehryar M <shehryarm1029@gmail.com>
2025-02-06 11:41:50 -0500

    Added everything else

---
+++ client/src/App.tsx
@@ -0,0 +1,13 @@
+import { useState } from "react";
+import axios from "axios";
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <div></div>
+    </>
+  );
+}
+
+export default App;
