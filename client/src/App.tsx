8d19dba25bbfd1e299dedb14de4cc54bc123e0b7
Shehryar M <shehryarm1029@gmail.com>
2025-02-10 19:02:56 -0500

    Added input for uploading image and set up route for image generation

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -8,6 +8,10 @@ function App() {
   // i must include an empty array including the key I will be retrieving in userData {members: []}
   // without the key, the data will not be assigned properly in userData object
   const [userData, setUserData] = useState({ members: [] });
+  const [uploadedImage, setUploadedImage] = useState();
+  const [generatedImage, setGeneratedImage] = useState<string | undefined>(
+    undefined
+  );
 
   useEffect(() => {
     axios.get("http://127.0.0.1:5000/members").then((response) => {
@@ -30,13 +34,43 @@ function App() {
       });
   };
 
+  const onSubmit = () => {
+    if (!uploadedImage) {
+      console.error("No image selected!");
+      return;
+    }
+
+    const formData = new FormData();
+    formData.append("image", uploadedImage);
+    axios
+      .post("http://127.0.0.1:5000/generate", formData, {
+        responseType: "blob",
+      })
+      .then((response) => {
+        console.log(response);
+        const imageUrl = URL.createObjectURL(response.data);
+        setGeneratedImage(imageUrl); // Store the image URL in state
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+  };
+
+  const getImage = (e: any) => {
+    console.log(e.target.files);
+    setUploadedImage(e.target.files[0]);
+  };
+
   return (
     <>
       {/* A .map function was used because it modifies an array and returns a new one */}
       {userData.members.map((member, index) => (
         <div key={index}>{member}</div>
       ))}
-      <button onClick={onTest}>GENERATE</button>
+      <button onClick={onTest}>TEST</button>
+      <input type="file" name="file" onChange={getImage}></input>
+      <button onClick={onSubmit}>GENERATE</button>
+      {generatedImage && <img src={generatedImage} alt="Generated" />}
     </>
   );
 }
