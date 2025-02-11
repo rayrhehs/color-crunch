0d18cd67521bf5f99d21818b26b0bdb777804915
Shehryar M <shehryarm1029@gmail.com>
2025-02-11 10:57:20 -0500

    Cleaned up App.tsx code

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,38 +1,12 @@
-import { useState, useEffect } from "react";
+import { useState } from "react";
 import axios from "axios";
 import "./App.css";
 
 function App() {
-  // observe this object: {"members": ["Member1", "Member2", "Member3"]}
-  // it has a key called members has a value of an array
-  // i must include an empty array including the key I will be retrieving in userData {members: []}
-  // without the key, the data will not be assigned properly in userData object
-  const [userData, setUserData] = useState({ members: [] });
   const [uploadedImage, setUploadedImage] = useState();
   const [generatedImage, setGeneratedImage] = useState<string | undefined>(
     undefined
   );
-
-  // useEffect(() => {
-  //   axios.get("http://127.0.0.1:5000/members").then((response) => {
-  //     setUserData(response.data);
-  //   });
-  // }, []);
-
-  // // test
-  // const onTest = () => {
-  //   axios
-  //     .post("http://127.0.0.1:5000/test", { message: "Hello World" })
-  //     .then((response) => {
-  //       console.log(JSON.stringify(response));
-  //       console.log(
-  //         "Post request sent! This is the response: " + response.data
-  //       );
-  //     })
-  //     .catch((error) => {
-  //       console.error(error);
-  //     });
-  // };
 
   const onSubmit = () => {
     if (!uploadedImage) {
@@ -41,6 +15,8 @@ function App() {
     }
 
     const formData = new FormData();
+
+    // find out what FORMDATA DOES and why its important
     formData.append("image", uploadedImage);
 
     axios
@@ -63,11 +39,6 @@ function App() {
 
   return (
     <>
-      {/* A .map function was used because it modifies an array and returns a new one */}
-      {/* {userData.members.map((member, index) => (
-        <div key={index}>{member}</div>
-      ))} */}
-      {/* <button onClick={onTest}>TEST</button> */}
       <div
         style={{
           display: "flex",
